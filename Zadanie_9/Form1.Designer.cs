2bfd2bae337951735696f7329bb8fcb2b90d39ed
Leslaw1589 <jokerostyczny@gmail.com>
2021-08-25 19:52:04 +0200

    Add project files.

---
+++ Zadanie_9/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace Zadanie_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numLength = new System.Windows.Forms.NumericUpDown();
+            this.numPass = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numLength)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPass)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(97, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Długość hasła:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(240, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Stopień skomplikowania:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(464, 52);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Generuj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Wygenerowane hasło to:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(13, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // numLength
+            // 
+            this.numLength.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numLength.Location = new System.Drawing.Point(258, 9);
+            this.numLength.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numLength.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numLength.Name = "numLength";
+            this.numLength.Size = new System.Drawing.Size(120, 27);
+            this.numLength.TabIndex = 7;
+            this.numLength.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // numPass
+            // 
+            this.numPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numPass.Location = new System.Drawing.Point(258, 42);
+            this.numPass.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numPass.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numPass.Name = "numPass";
+            this.numPass.Size = new System.Drawing.Size(120, 27);
+            this.numPass.TabIndex = 8;
+            this.numPass.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 322);
+            this.Controls.Add(this.numPass);
+            this.Controls.Add(this.numLength);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Generator haseł";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numLength)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPass)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numLength;
+        private System.Windows.Forms.NumericUpDown numPass;
+    }
+}
+
---
+++ Zadanie_9/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label3.Visible = false;
+            label4.Visible = false;
+            numLength.Value = 5;
+            numPass.Value = 2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string password = generate(numLength.Value, numPass.Value);
+
+            label4.Text = password;
+
+            if (!label3.Visible) label3.Visible = true;
+            if (!label4.Visible) label4.Visible = true;
+
+        }
+
+        private string generate(decimal length, decimal hardness)
+        {
+            string pass = "";
+
+            Random r = new Random();
+
+            int Randfunc(int min, int max)
+            {
+                int n = r.Next(min, max);
+                return n;
+            }
+
+            switch (hardness)
+            {
+                case 1:
+                    {
+                        for (int i = 0; i < length; i++)
+                        {
+                            pass += Convert.ToChar(Randfunc(48, 57));
+                        }
+                        return pass;
+                    }
+                case 2:
+                    {
+                        for (int i = 0; i < length; i++)
+                        {
+                            switch (Randfunc(1, 3))
+                            {
+                                case 1:
+                                    {
+                                        pass += Convert.ToChar(Randfunc(48, 57));
+                                        break;
+                                    }
+                                case 2:
+                                    {
+                                        pass += Convert.ToChar(Randfunc(65, 90));
+                                        break;
+                                    }
+                                case 3:
+                                    {
+                                        pass += Convert.ToChar(Randfunc(97, 122));
+                                        break;
+                                    }
+                            }
+                        }
+                        return pass;
+                    }
+                case 3:
+                    {
+                        for (int i = 0; i < length; i++)
+                        {
+                            pass += Convert.ToChar(Randfunc(33, 126));
+                        }
+                        return pass;
+                    }
+
+                default: return pass;
+            }
+        }
+    }
+}
